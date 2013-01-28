c86475e970a45ac4436f95313e35c4d71d6eac8d
Devin Nusbaum <d.w.nusbaum@gmail.com>
2013-01-27 22:09:26 -0500

    Better formatting of dates

--- Log.hs
+++ Log.hs
@@ -7,19 +7,26 @@ module Log
     ) where
 
 import Data.Time.Calendar
+import Data.Time.Format
+import System.Locale
 
 data Log = Log String Day [Entry]
 
 data Entry = Entry Day String
 
 instance Show Log where
-    show (Log n d xs) = n ++ "\n" ++ "Created on: " ++ showLn d ++ unwords (map showLn xs)
+    show (Log n d xs) = n ++ " log" ++ "\n" ++ "Created on: " ++ showDay d ++ "\n" ++ unwords (map showLn xs)
 
 instance Show Entry where
-    show (Entry d s) = "|> " ++ showLn d ++ s
+    show (Entry d s) = showDay d ++ "\n|> " ++ s
 
 showLn :: (Show a) => a -> String
 showLn s = show s ++ "\n"
 
+showDay :: Day -> String
+showDay = formatTime defaultTimeLocale "%a %d %Y - %I:%M%p"
+
 makeEntry :: [String] -> Day -> Entry
 makeEntry e d = Entry d $ unwords e
+
+
