1fe476acd768215af2701d6fd1c72552e703fb35
Devin Nusbaum <d.w.nusbaum@gmail.com>
2013-02-02 20:45:36 -0500

    Added some error handling

--- Logger.hs
+++ Logger.hs
@@ -33,7 +33,7 @@ editLog n = do
     eitherLog <- liftIO $ tryIOError $ openLog n AppendMode
     case eitherLog of
         Left  e -> when (isDoesNotExistError e) $ liftIO (newLog n) >> editLog n
-        Right h -> getInputLine "<Log entry>: " >>= \i -> case i of
+        Right h -> getInputLine "> " >>= \i -> case i of
             Nothing -> return ()
             Just i' -> liftIO $ getZonedTime >>= hAppendLog h . flip Entry i'
 
@@ -61,11 +61,11 @@ helpLog "view" = putStrLn
     \Action: Displays any log file with the given name"
 helpLog _ = putStrLn
     "Usage:\n\
-    \log edit   <logs>    Edit the given log file\n\
+    \log edit   <logs>    Edit the given log file.\n\
     \log help   <command> If no command is given, display this message.\n\
     \                     Otherwise, display specific help message for a command.\n\
-    \log new    <names>   Creates a log file with the given name\n\
-    \log remove <logs>    Removes the given log file"
+    \log new    <names>   Creates a log file with the given name.\n\
+    \log remove <logs>    Removes the given log file."
 
 --Creating logs
 
@@ -75,17 +75,25 @@ newLog n = openLog n ReadWriteMode >>= \h -> hPutStrLn h n >> hClose h
 --Removing logs
 
 removeLog :: String -> InputT IO ()
-removeLog n = liftIO (logsFolder n) >>= ensureRemoveFile
-
-ensureRemoveFile :: FilePath -> InputT IO ()
-ensureRemoveFile f = do
-    i <- getInputLine $ "Are you sure you want to remove \'" ++ f ++ "\'? Enter (Y)es or (N)o: "
+removeLog n = do
+    f <- liftIO $ logsFolder n
+    i <- getInputLine $ "Are you sure you want to remove log " ++ show n ++ "? Enter (Y)es or (N)o: "
     case i of
-        Just "Yes" -> liftIO $ removeFile f
-        Just "Y"   -> liftIO $ removeFile f
+        Just "Yes" -> liftIO $ safeRemove f
+        Just "Y"   -> liftIO $ safeRemove f
         _          -> return ()
+  where safeRemove f = do
+            eitherError <- tryIOError $ removeFile f
+            case eitherError of
+                Left  e -> when (isDoesNotExistError e) $ putStrLn $ "Log " ++ show n ++ " does not exist"
+                Right _ -> return ()
 
 --Viewing Logs
 
 viewLog :: String -> IO ()
-viewLog n = logsFolder n >>= flip openFile ReadMode >>= \h -> hGetContents h >>= putStrLn >> hClose h
+viewLog n = do
+    f <- logsFolder n
+    eitherHandle <- tryIOError $ openFile f ReadMode
+    case eitherHandle of
+        Left  e -> when (isDoesNotExistError e) $ putStrLn $ "Log " ++ show n ++ " does not exist"
+        Right h -> hGetContents h >>= putStrLn >> hClose h
